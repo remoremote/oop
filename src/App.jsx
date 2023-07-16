9f02193fde93fe55c8424351512dde147f59a638
Remo <81964236+remoremote@users.noreply.github.com>
2023-07-16 11:37:32 +0200

    initial commit to set up app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>This is Home!</div>,
+  },
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+  return
+    <div>
+      <RouterProvider router={router}>
+    </div>;
 }
 
 export default App;
